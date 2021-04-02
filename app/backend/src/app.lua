c2850dfe4e044eac278c62c5dd0a4c40a2889687
Debian <debian@vps-20042c44.vps.ovh.net>
2021-04-02 22:23:44 +0000

    4040 error

--- app/backend/src/app.lua
+++ app/backend/src/app.lua
@@ -1,6 +1,16 @@
 local lapis = require 'lapis'
 local app = lapis.Application()
+local get_error = require('utils.error').get_error
 app.include = function(self, a) self.__class.include(self, a, nil, self) end
+
+do
+	function app.handle_404()
+		return {
+			status = tonumber(tostring(get_error.endpoint_not_found().errorCode):sub(1,3)),
+			json = get_error.endpoint_not_found()
+		}
+	end
+end
 
 app:include('routes')
 
